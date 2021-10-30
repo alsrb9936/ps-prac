e98980e189cb6b4e881a881108faf16713f1a1df
MinGyu <ghfn123@gmail.com>
2021-10-30 17:09:59 +0900

    ADD BOJ10871

---
+++ barkingdog/lecture_2/boj10871.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+    int N,X;
+    int a;
+    cin >> N >> X;
+
+    for(int i=0;i<N;i++){
+        cin >> a;
+        if(a<N) cout << a << ' ';
+    }
+
+}
