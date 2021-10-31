3335a5b7f4f51866347db622d00b46916b87ef9e
MinGyu <ghfn123@gmail.com>
2021-10-31 17:10:27 +0900

    ADD BOJ1080 4

--- barkingdog/lecture_2/boj10804_카드역배치.cpp
+++ barkingdog/lecture_2/boj10804_카드역배치.cpp
@@ -3,8 +3,10 @@
 using namespace std;
 
 int main(){
+    ios::sync_with_stdio(0);cin.tie(0);
     int a,b;
     int arr[20];
+
     for(int i=0;i<20;i++) arr[i] = i+1;
 
     for(int i=0;i<10;i++){
