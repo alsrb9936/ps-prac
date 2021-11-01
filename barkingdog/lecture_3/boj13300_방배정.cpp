17da88adc56d77079fe59c40d61fb0041644168f
MinGyu <ghfn123@gmail.com>
2021-11-02 00:10:52 +0900

    ADD BOJ13300

---
+++ barkingdog/lecture_3/boj13300_방배정.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int man[7];
+int woman[7];
+
+int Allocate_Room(int K){
+    int cnt = 0;
+    for(int i=0;i<7;i++){
+        if(man[i]/K > 0) cnt += man[i]/K;
+        if(man[i]%K > 0) cnt += 1;
+        if(woman[i]/K > 0) cnt += woman[i]/K;
+        if(woman[i]%K > 0) cnt += 1;
+    }
+
+    return cnt;
+}
+int main(){
+    int N,K,gender,grade;
+    cin >> N >> K;
+
+    for(int i=0;i<N;i++){
+        cin >> gender >> grade;
+        if(gender) man[grade]++;
+        else woman[grade]++;
+    }
+
+    cout << Allocate_Room(K);
+
+}
